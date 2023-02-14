ed3e51997f4c308690ffa7c2830b3b541b9ad5e0
raguramMurugan <raguram.m@hubino.com>
2023-02-14 18:56:04 +0530

    First Commit

---
+++ EmployeeHandler.java
@@ -0,0 +1,7 @@
+package com.hubino.awslambda.awsDemo.handler;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class EmployeeHandler extends SpringBootRequestHandler<String, Object>{
+
+}
